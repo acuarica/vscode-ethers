0d2a8a52165d7a3c1e280b5b29e3c435b287d291
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-04-13 12:53:13 +0200

    Add draft for querying contract

---
+++ src/CodelensProvider.ts
@@ -0,0 +1,74 @@
+import * as vscode from 'vscode';
+
+
+import { Contract, ethers } from "ethers";
+
+
+/**
+ * CodelensProvider
+ */
+export class CodelensProvider implements vscode.CodeLensProvider {
+
+    private codeLenses: vscode.CodeLens[] = [];
+    private regex: RegExp;
+    private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+    public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
+
+    constructor() {
+        this.regex = /(.+)/g;
+
+        vscode.workspace.onDidChangeConfiguration((_) => {
+            this._onDidChangeCodeLenses.fire();
+        });
+    }
+
+    public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+
+        if (vscode.workspace.getConfiguration("codelens-sample").get("enableCodeLens", true)) {
+            this.codeLenses = [];
+            const regex = new RegExp(this.regex);
+            const text = document.getText();
+            let matches;
+            let currentAddress: string | null = null;
+            while ((matches = regex.exec(text)) !== null) {
+                const line = document.lineAt(document.positionAt(matches.index).line);
+                const indexOf = line.text.indexOf(matches[0]);
+                const position = new vscode.Position(line.lineNumber, indexOf);
+                const range = document.getWordRangeAtPosition(position, new RegExp(this.regex));
+
+                if (range) {
+                    if (line.text.startsWith('function') && currentAddress) {
+                        this.codeLenses.push(new vscode.CodeLens(range, {
+                            title: 'call method',
+                            command: 'codelens-sample.codelensAction',
+                            arguments: [currentAddress, line.text],
+                        }));
+                    } else {
+                        currentAddress = line.text;
+                        this.codeLenses.push(new vscode.CodeLens(range, {
+                            title: 'Contract',
+                            command: 'codelens-sample.codelensAction',
+                            arguments: [line.text],
+                        }));
+                    }
+                }
+            }
+            return this.codeLenses;
+        }
+        return [];
+    }
+
+    // public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken) {
+    //     if (vscode.workspace.getConfiguration("codelens-sample").get("enableCodeLens", true)) {
+    //         codeLens.command = {
+    //             title: "Codelens provided by sample extension",
+    //             tooltip: "Tooltip provided by sample extension",
+    //             command: "codelens-sample.codelensAction",
+    //             arguments: ["Argument 1", codeLens.range.]
+    //         };
+    //         return codeLens;
+    //     }
+    //     return null;
+    // }
+}
+
---
+++ src/extension.ts
@@ -0,0 +1,54 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { Contract, ethers } from 'ethers';
+import { Fragment, FunctionFragment, Interface } from 'ethers/lib/utils';
+import { ExtensionContext, languages, commands, Disposable, workspace, window } from 'vscode';
+import { CodelensProvider } from './CodelensProvider';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+let disposables: Disposable[] = [];
+
+export function activate(context: ExtensionContext) {
+    const codelensProvider = new CodelensProvider();
+
+    languages.registerCodeLensProvider("*", codelensProvider);
+
+    commands.registerCommand("codelens-sample.enableCodeLens", () => {
+        workspace.getConfiguration("codelens-sample").update("enableCodeLens", true, true);
+    });
+
+    commands.registerCommand("codelens-sample.disableCodeLens", () => {
+        workspace.getConfiguration("codelens-sample").update("enableCodeLens", false, true);
+    });
+
+    commands.registerCommand("codelens-sample.codelensAction", async (contractAddress: string, funcSig: any) => {
+
+        console.log(contractAddress);
+        console.log(funcSig);
+
+        const func = Fragment.from(funcSig);
+        // console.log(.functions);
+        // const funcName = func.name;
+
+        console.log(func.name);
+
+        const provider = new ethers.providers.JsonRpcProvider(' https://api.avax-test.network/ext/bc/C/rpc');
+        const contract = new Contract(contractAddress, [func], provider);
+        // window.
+
+        const result = await contract.functions[func.name]();
+
+        window.showInformationMessage(`CodeLens action clicked with args=${result}`);
+
+    });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+    if (disposables) {
+        disposables.forEach(item => item.dispose());
+    }
+    disposables = [];
+}
