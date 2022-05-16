00bf586e97f7de4c1ffa166082220ae5dc92fa45
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-05-16 18:33:31 +0200

    Add ts docs to `cashflow` module

--- src/cashflow.ts
+++ src/cashflow.ts
@@ -8,6 +8,14 @@ import { BlockRange } from "./parse";
  */
 export type Balances = { [from: string]: BigNumber };
 
+/**
+ * Fetch and flatten transactions from blocks specified in the `blockRange`.
+ * 
+ * @param provider the provider to use to fetch the current block number when `to` it is not specified.
+ * @param getBlock the actual function to use to fetch blocks.
+ * @param blockRange specifies the `from` and optional `to` block numbers to fetch transactions from.
+ * @returns the list of transactions contained in the specified block range.
+ */
 export async function fetchTransactions(provider: providers.Provider, getBlock: (block: number) => Promise<BlockWithTransactions>, blockRange: BlockRange): Promise<Transaction[]> {
     if (blockRange.to === undefined) {
         blockRange.to = await provider.getBlockNumber();
@@ -35,6 +43,14 @@ export async function fetchTransactions(provider: providers.Provider, getBlock: 
     return transactions;
 }
 
+/**
+ * Calculates the Ether Cash Flow from the list of `transactions`.
+ * Each transaction in `transactions` is just a slice of the transaction object returned by Ethers.js.
+ * 
+ * @param transactions the list of transactions to calculate the Ether Cash Flow.
+ * @returns the `total` of Ether transferred,
+ * and the list of `senders` and `receivers` the participated in the list of transactions.
+ */
 export function cashFlow(transactions:
     {
         from?: string,
@@ -59,9 +75,30 @@ export function cashFlow(transactions:
     }
 
     return { total, senders, receivers };
+
+    /**
+     * Updates the `balances` by adding or updating the `address` by `amount`.
+     * 
+     * @param balances the map to add or update the `address` by `amount`.
+     * @param address the address that sent or received Ether.
+     * @param amount the amount of Ether that was sent or received.
+     */
+    function updateBalance(balances: Balances, address: string, amount: BigNumber) {
+        const previousAmount = balances[address] ?? constants.Zero;
+        balances[address] = previousAmount.add(amount);
+    }
 }
 
-function updateBalance(balances: Balances, address: string, amount: BigNumber) {
-    const previousAmount = balances[address] ?? constants.Zero;
-    balances[address] = previousAmount.add(amount);
+/**
+ * Checks whethen the given `address` is a contract address.
+ * 
+ * @param provider the provider to use to fetch the code of the given `address`.
+ * @param address the address to query.
+ * @returns `true` is the given `address` is a contract address.
+ * This is defined by having its code different from `0x`.
+ * Otherwise, returns `false`.
+ */
+export async function isContract(provider: providers.Provider, address: string): Promise<boolean> {
+    const code = await provider.getCode(address);
+    return code !== '0x';
 }
