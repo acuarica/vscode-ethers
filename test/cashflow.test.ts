8e7f2c0b2639435b71b31e959f45fb155807f08f
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-05-17 22:44:40 +0200

    Add more cash flow test cases

--- test/cashflow.test.ts
+++ test/cashflow.test.ts
@@ -4,6 +4,118 @@ import { cashFlow, fetchTransactions } from "../src/lib/cashflow";
 import { providers } from "ethers";
 import ganache from "ganache";
 
+const provider = new providers.Web3Provider(ganache.provider({
+    logging: {
+        quiet: true
+    },
+    fork: {
+        url: 'https://api.avax-test.network/ext/bc/C/rpc',
+        blockNumber: 972394,
+    }
+}) as any);
+
+describe('fetchTransactions', function () {
+
+    this.timeout(5000);
+
+    it('should fetch latest transactions', async () => {
+        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972390, to: 972393 });
+        expect(transactions.length).to.be.equal(33);
+    });
+
+    it('should fetch latest transactions with inverted range', async () => {
+        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972393, to: 972390 });
+        expect(transactions.length).to.be.equal(33);
+    });
+
+    it('should fetch latest transactions without specifying `to` block', async () => {
+        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972390 });
+        expect(transactions.length).to.be.equal(42);
+    });
+
+});
+
+describe('cashFlow', () => {
+
+    it('should return cash flow', () => {
+        const txs = [
+            { from: '0xa', to: '0xb', value: '2'.bn() },
+            { from: '0xb', to: '0xc', value: '5'.bn() },
+            { from: '0xf', to: '0xf', value: '10'.bn() },
+        ];
+        const report = cashFlow(txs);
+        expect(report).to.be.deep.equal({
+            total: '17'.bn(),
+            senders: {
+                '0xa': '2'.bn(),
+                '0xb': '5'.bn(),
+                '0xf': '10'.bn(),
+            },
+            receivers: {
+                '0xb': '2'.bn(),
+                '0xc': '5'.bn(),
+                '0xf': '10'.bn(),
+            }
+        });
+    });
+
+    it('should allow addresses to be sender & receiver in the same transaction', () => {
+        const txs = [
+            { from: '0xa', to: '0xb', value: '2'.bn() },
+            { from: '0xf', to: '0xf', value: '10'.bn() },
+        ];
+        const report = cashFlow(txs);
+        expect(report).to.be.deep.equal({
+            total: '12'.bn(),
+            senders: {
+                '0xa': '2'.bn(),
+                '0xf': '10'.bn(),
+            },
+            receivers: {
+                '0xb': '2'.bn(),
+                '0xf': '10'.bn(),
+            }
+        });
+    });
+
+    it('should update balances for addresses that send or receive multiple times', () => {
+        const txs = [
+            { from: '0xa', to: '0xb', value: '1'.bn() },
+            { from: '0xb', to: '0xc', value: '2'.bn() },
+            { from: '0xa', to: '0xd', value: '3'.bn() },
+            { from: '0xe', to: '0xc', value: '4'.bn() },
+        ];
+        const report = cashFlow(txs);
+        expect(report).to.be.deep.equal({
+            total: '10'.bn(),
+            senders: {
+                '0xa': '4'.bn(),
+                '0xb': '2'.bn(),
+                '0xe': '4'.bn(),
+            },
+            receivers: {
+                '0xb': '1'.bn(),
+                '0xc': '6'.bn(),
+                '0xd': '3'.bn(),
+            }
+        });
+    });
+
+    it('should return cash from from `txs`', () => {
+        const flow = cashFlow(txs);
+        expect(flow.total).to.be.deep.equal(BigNumber.from('30300000000000000000'));
+    });
+
+});
+
+declare global {
+    interface String {
+        bn(this: string): BigNumber;
+    }
+}
+
+String.prototype.bn = function (this: string) { return BigNumber.from(this); };
+
 const txs = [
     {
         from: '0x454ee4Ac5e8f6c3633f3470bBe5dD1650741103f',
@@ -51,41 +163,3 @@ const txs = [
         value: BigNumber.from('0x00')
     }
 ];
-
-const provider = new providers.Web3Provider(ganache.provider({
-    logging: {
-        quiet: true
-    },
-    fork: {
-        url: 'https://api.avax-test.network/ext/bc/C/rpc',
-        blockNumber: 972394,
-    }
-}) as any);
-
-describe('fetchTransactions', function () {
-
-    this.timeout(5000);
-
-    it('should fetch latest transactions', async () => {
-        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972390, to: 972393 });
-        expect(transactions.length).to.be.equal(33);
-    });
-
-    it('should fetch latest transactions with inverted range', async () => {
-        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972393, to: 972390 });
-        expect(transactions.length).to.be.equal(33);
-    });
-
-    it('should fetch latest transactions without specifying `to` block', async () => {
-        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972390 });
-        expect(transactions.length).to.be.equal(42);
-    });
-
-});
-
-describe('cashFlow', () => {
-    it('should resolve method calls', () => {
-        const flow = cashFlow(txs);
-        expect(flow.total).to.be.deep.equal(BigNumber.from('30300000000000000000'));
-    });
-});
