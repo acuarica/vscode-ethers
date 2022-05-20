e9f674fe7951417b21f18d3d7e7d678071fe0118
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-05-20 12:13:13 +0200

    Add module level test description for better org

--- test/cashflow.test.ts
+++ test/cashflow.test.ts
@@ -15,96 +15,102 @@ const provider = new providers.Web3Provider(ganache.provider({
     }
 }) as any);
 
-describe('fetchTransactions', function () {
+describe('cashflow', function () {
 
-    this.timeout(5000);
+    describe('fetchTransactions', function () {
 
-    it('should fetch latest transactions', async () => {
-        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972390, to: 972393 });
-        expect(transactions.length).to.be.equal(33);
-    });
+        this.timeout(5000);
 
-    it('should fetch latest transactions with inverted range', async () => {
-        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972393, to: 972390 });
-        expect(transactions.length).to.be.equal(33);
-    });
+        const getBlock = (block: number) => provider.getBlockWithTransactions(block);
 
-    it('should fetch latest transactions without specifying `to` block', async () => {
-        const transactions = await fetchTransactions(provider, (block: number) => provider.getBlockWithTransactions(block), { from: 972390 });
-        expect(transactions.length).to.be.equal(42);
-    });
-
-});
-
-describe('cashFlow', () => {
-
-    it('should return cash flow', () => {
-        const txs = [
-            { from: '0xa', to: '0xb', value: '2'.bn() },
-            { from: '0xb', to: '0xc', value: '5'.bn() },
-            { from: '0xf', to: '0xf', value: '10'.bn() },
-        ];
-        const report = cashFlow(txs);
-        expect(report).to.be.deep.equal({
-            total: '17'.bn(),
-            senders: {
-                '0xa': '2'.bn(),
-                '0xb': '5'.bn(),
-                '0xf': '10'.bn(),
-            },
-            receivers: {
-                '0xb': '2'.bn(),
-                '0xc': '5'.bn(),
-                '0xf': '10'.bn(),
-            }
+        it('should fetch latest transactions', async () => {
+            const transactions = await fetchTransactions(provider, getBlock, { from: 972390, to: 972393 });
+            expect(transactions.length).to.be.equal(33);
         });
-    });
 
-    it('should allow addresses to be sender & receiver in the same transaction', () => {
-        const txs = [
-            { from: '0xa', to: '0xb', value: '2'.bn() },
-            { from: '0xf', to: '0xf', value: '10'.bn() },
-        ];
-        const report = cashFlow(txs);
-        expect(report).to.be.deep.equal({
-            total: '12'.bn(),
-            senders: {
-                '0xa': '2'.bn(),
-                '0xf': '10'.bn(),
-            },
-            receivers: {
-                '0xb': '2'.bn(),
-                '0xf': '10'.bn(),
-            }
+        it('should fetch latest transactions with inverted range', async () => {
+            const transactions = await fetchTransactions(provider, getBlock, { from: 972393, to: 972390 });
+            expect(transactions.length).to.be.equal(33);
         });
-    });
 
-    it('should update balances for addresses that send or receive multiple times', () => {
-        const txs = [
-            { from: '0xa', to: '0xb', value: '1'.bn() },
-            { from: '0xb', to: '0xc', value: '2'.bn() },
-            { from: '0xa', to: '0xd', value: '3'.bn() },
-            { from: '0xe', to: '0xc', value: '4'.bn() },
-        ];
-        const report = cashFlow(txs);
-        expect(report).to.be.deep.equal({
-            total: '10'.bn(),
-            senders: {
-                '0xa': '4'.bn(),
-                '0xb': '2'.bn(),
-                '0xe': '4'.bn(),
-            },
-            receivers: {
-                '0xb': '1'.bn(),
-                '0xc': '6'.bn(),
-                '0xd': '3'.bn(),
-            }
+        it('should fetch latest transactions without specifying `to` block', async () => {
+            const transactions = await fetchTransactions(provider, getBlock, { from: 972390 });
+            expect(transactions.length).to.be.equal(42);
         });
+
     });
 
-    it('should return cash from from `txs`', () => {
-        const flow = cashFlow(txs);
-        expect(flow.total).to.be.deep.equal(BigNumber.from('30300000000000000000'));
+    describe('cashFlow', () => {
+
+        it('should return cash flow', () => {
+            const txs = [
+                { from: '0xa', to: '0xb', value: '2'.bn() },
+                { from: '0xb', to: '0xc', value: '5'.bn() },
+                { from: '0xf', to: '0xf', value: '10'.bn() },
+            ];
+            const report = cashFlow(txs);
+            expect(report).to.be.deep.equal({
+                total: '17'.bn(),
+                senders: {
+                    '0xa': '2'.bn(),
+                    '0xb': '5'.bn(),
+                    '0xf': '10'.bn(),
+                },
+                receivers: {
+                    '0xb': '2'.bn(),
+                    '0xc': '5'.bn(),
+                    '0xf': '10'.bn(),
+                }
+            });
+        });
+
+        it('should allow addresses to be sender & receiver in the same transaction', () => {
+            const txs = [
+                { from: '0xa', to: '0xb', value: '2'.bn() },
+                { from: '0xf', to: '0xf', value: '10'.bn() },
+            ];
+            const report = cashFlow(txs);
+            expect(report).to.be.deep.equal({
+                total: '12'.bn(),
+                senders: {
+                    '0xa': '2'.bn(),
+                    '0xf': '10'.bn(),
+                },
+                receivers: {
+                    '0xb': '2'.bn(),
+                    '0xf': '10'.bn(),
+                }
+            });
+        });
+
+        it('should update balances for addresses that send or receive multiple times', () => {
+            const txs = [
+                { from: '0xa', to: '0xb', value: '1'.bn() },
+                { from: '0xb', to: '0xc', value: '2'.bn() },
+                { from: '0xa', to: '0xd', value: '3'.bn() },
+                { from: '0xe', to: '0xc', value: '4'.bn() },
+            ];
+            const report = cashFlow(txs);
+            expect(report).to.be.deep.equal({
+                total: '10'.bn(),
+                senders: {
+                    '0xa': '4'.bn(),
+                    '0xb': '2'.bn(),
+                    '0xe': '4'.bn(),
+                },
+                receivers: {
+                    '0xb': '1'.bn(),
+                    '0xc': '6'.bn(),
+                    '0xd': '3'.bn(),
+                }
+            });
+        });
+
+        it('should return cash from from `txs`', () => {
+            const flow = cashFlow(txs);
+            expect(flow.total).to.be.deep.equal(BigNumber.from('30300000000000000000'));
+        });
+
     });
 
 });
--- test/provider.test.ts
+++ test/provider.test.ts
@@ -12,108 +12,112 @@ import '../src/lib/str';
 use(chaiAsPromised);
 use(waffleChai);
 
-describe('execCall', function () {
+describe('provider', function () {
 
-    this.timeout(4000);
+    describe('execCall', function () {
 
-    let provider: providers.Provider;
-    let ldtokenAddress: string;
+        this.timeout(4000);
 
-    const createProvider = (_network: string) => provider;
+        let provider: providers.Provider;
+        let ldtokenAddress: string;
 
-    const privateKey = '0x7f109a9e3b0d8ecfba9cc23a3614433ce0fa7ddcc80f2a8f10b222179a5a80d6';
+        const createProvider = (_network: string) => provider;
 
-    before(async () => {
-        provider = new providers.Web3Provider(ganache.provider({
-            quiet: true,
-            wallet: {
-                accounts: [
-                    { balance: '0xffffffffffffffffffffff', secretKey: privateKey }
-                ],
+        const privateKey = '0x7f109a9e3b0d8ecfba9cc23a3614433ce0fa7ddcc80f2a8f10b222179a5a80d6';
+
+        before(async () => {
+            provider = new providers.Web3Provider(ganache.provider({
+                quiet: true,
+                wallet: {
+                    accounts: [
+                        { balance: '0xffffffffffffffffffffff', secretKey: privateKey }
+                    ],
+                }
+            }) as any);
+
+            const deployer = new Wallet(privateKey, provider);
+            expect(await provider.getBalance(deployer.address)).to.be.deep.equal('0xffffffffffffffffffffff'.bn());
+
+            const factory = new ContractFactory(deposit.abi, deposit.bytecode, deployer);
+            const contract = await factory.deploy('LD Token', 'LDT');
+            ldtokenAddress = contract.address;
+        });
+
+        it('should return value from view calls', async () => {
+            const mode = new EthersMode();
+            mode.net('_unused');
+            mode.address(ldtokenAddress.asAddress());
+
+            {
+                const call = mode.call('name() view returns (string)'.asCall());
+                expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['LD Token']);
             }
-        }) as any);
+            {
+                const call = mode.call('symbol() view returns (string)'.asCall());
+                expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['LDT']);
+            }
+            {
+                const call = mode.call(`balanceOf(${computeAddress(privateKey)}) view returns (uint256)`.asCall());
+                expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['0'.bn()]);
+            }
+        });
 
-        const deployer = new Wallet(privateKey, provider);
-        expect(await provider.getBalance(deployer.address)).to.be.deep.equal('0xffffffffffffffffffffff'.bn());
+        it('should reject call when network is not provided', async () => {
+            const mode = new EthersMode();
+            mode.address(ldtokenAddress.asAddress());
 
-        const factory = new ContractFactory(deposit.abi, deposit.bytecode, deployer);
-        const contract = await factory.deploy('LD Token', 'LDT');
-        ldtokenAddress = contract.address;
-    });
+            {
+                const call = mode.call('name() view returns (string)'.asCall());
+                await expect(execCall(call.resolve(), createProvider))
+                    .to.be.eventually.rejectedWith('No network provided');
+            }
+        });
 
-    it('should return value from view calls', async () => {
-        const mode = new EthersMode();
-        mode.net('_unused');
-        mode.address(ldtokenAddress.asAddress());
+        it('should return value from view calls using alias and `this`', async () => {
+            const mode = new EthersMode();
+            mode.net('_unused');
+            mode.address(`${ldtokenAddress} as token`.asAddress());
 
-        {
-            const call = mode.call('name() view returns (string)'.asCall());
-            expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['LD Token']);
-        }
-        {
-            const call = mode.call('symbol() view returns (string)'.asCall());
-            expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['LDT']);
-        }
-        {
-            const call = mode.call(`balanceOf(${computeAddress(privateKey)}) view returns (uint256)`.asCall());
-            expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['0'.bn()]);
-        }
-    });
+            mode.address(`${computeAddress(privateKey)}`.asAddress());
+            {
+                const call = mode.call('token.balanceOf(this) view returns (uint256)'.asCall());
+                expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['0'.bn()]);
+            }
+        });
 
-    it('should errreturn value from view calls using alias and `this`', async () => {
-        const mode = new EthersMode();
-        mode.address(ldtokenAddress.asAddress());
+        it('should sign transaction using signers', async () => {
+            const mode = new EthersMode();
+            mode.net('_unused');
+            mode.address(`${ldtokenAddress} as token`.asAddress());
 
-        {
-            const call = mode.call('name() view returns (string)'.asCall());
-            await expect(execCall(call.resolve(), createProvider))
-                .to.be.eventually.rejectedWith('No network provided');
-        }
-    });
+            mode.address(`${privateKey}`.asAddress());
+            {
+                const call = mode.call('token.balanceOf(this) view returns (uint256)'.asCall());
+                expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['0'.bn()]);
+            }
+            {
+                const call = mode.call('token._mint(this, uint256 5000)'.asCall());
+                await execCall(call.resolve(), createProvider);
+            }
+            {
+                const call = mode.call('token.balanceOf(this) view returns (uint256)'.asCall());
+                expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['5000'.bn()]);
+            }
+        });
 
-    it('should return value from view calls using alias and `this`', async () => {
-        const mode = new EthersMode();
-        mode.net('_unused');
-        mode.address(`${ldtokenAddress} as token`.asAddress());
+        it('should reject transaction because account does not have sufficient funds for intrinsic cost', async () => {
+            const mode = new EthersMode();
+            mode.net('_unused');
 
-        mode.address(`${computeAddress(privateKey)}`.asAddress());
-        {
-            const call = mode.call('token.balanceOf(this) view returns (uint256)'.asCall());
-            expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['0'.bn()]);
-        }
-    });
+            mode.address(`${ldtokenAddress} as token`.asAddress());
+            mode.address(`0x0000000000000000000000000000000000000000000000000000000000000001`.asAddress());
+            {
+                const call = mode.call('token._mint(this, uint256 5000)'.asCall());
+                await expect(execCall(call.resolve(), createProvider))
+                    .to.be.eventually.rejectedWith('insufficient funds for intrinsic transaction cost');
+            }
+        });
 
-    it('should sign transaction using signers', async () => {
-        const mode = new EthersMode();
-        mode.net('_unused');
-        mode.address(`${ldtokenAddress} as token`.asAddress());
-
-        mode.address(`${privateKey}`.asAddress());
-        {
-            const call = mode.call('token.balanceOf(this) view returns (uint256)'.asCall());
-            expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['0'.bn()]);
-        }
-        {
-            const call = mode.call('token._mint(this, uint256 5000)'.asCall());
-            await execCall(call.resolve(), createProvider);
-        }
-        {
-            const call = mode.call('token.balanceOf(this) view returns (uint256)'.asCall());
-            expect(await execCall(call.resolve(), createProvider)).to.be.deep.equal(['5000'.bn()]);
-        }
-    });
-
-    it('should reject transaction because account does not have sufficient funds for intrinsic cost', async () => {
-        const mode = new EthersMode();
-        mode.net('_unused');
-
-        mode.address(`${ldtokenAddress} as token`.asAddress());
-        mode.address(`0x0000000000000000000000000000000000000000000000000000000000000001`.asAddress());
-        {
-            const call = mode.call('token._mint(this, uint256 5000)'.asCall());
-            await expect(execCall(call.resolve(), createProvider))
-                .to.be.eventually.rejectedWith('insufficient funds for intrinsic transaction cost');
-        }
     });
 
 });
