7794383721401ef5b832e1744a006e95f72d6345
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-05-20 16:52:55 +0200

    Add block's markdown

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,4 +1,5 @@
 import { ModeProvider } from "./provider";
+import { Block } from "@ethersproject/abstract-provider";
 
 /**
  * 
@@ -8,6 +9,17 @@ import { ModeProvider } from "./provider";
 export function getProviderMarkdown(provider: ModeProvider) {
     const explorerUrl = provider.explorerUrl ? `\n\n#### Explorer\n${provider.explorerUrl}` : '';
     const name = provider.name ? ` \`${provider.name}\`` : '';
-    const providerContent = `### Network${name}\n\n#### Connection\n${provider.connectionUrl}${explorerUrl}\n`;
-    return providerContent;
+    const content = `### Network${name}\n\n#### Connection\n${provider.connectionUrl}${explorerUrl}\n`;
+    return content;
+}
+
+/**
+ * 
+ * @param block 
+ * @returns 
+ */
+export function getBlockMarkdown(block: Pick<Block, 'number' | 'hash' | 'timestamp'>) {
+    const timestamp = new Date(block.timestamp * 1000).toUTCString();
+    const content = `### Block ${block.number}\n\n- Block Hash: \`${block.hash}\`\n- Timestamp: ${timestamp}\n`;
+    return content;
 }
