c0ac7a2a71768708fc5ba97aaaf5e0e8002b4824
Luis Mastrangelo <luismastrangelo@gmail.com>
2022-08-08 03:12:18 +0200

    Add filtering commands demo test

--- test/demo/suite/extension.test.ts
+++ test/demo/suite/extension.test.ts
@@ -1,45 +1,46 @@
 import * as assert from 'assert';
-import { after } from 'mocha';
+import { after, beforeEach } from 'mocha';
+import { commands, Position, Selection, TextEditor, TextEditorEdit, window, workspace } from 'vscode';
 
-import * as vscode from 'vscode';
-import { commands, Position, Selection, TextEditor } from 'vscode';
+const output = window.createOutputChannel('Ethers Mode - Demo');
+output.show(true);
+// output.appendLine('Here you see ');
 
-const wait = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
-
-async function typewrite(editor: TextEditor, start: Position, text: string, ms = 80) {
-    let pos = start;
-    for (const c of text) {
-        await editor.edit(
-            function (b: vscode.TextEditorEdit) {
-                b.insert(pos, c);
-            }
-        );
-        if (c === '\n') {
-            pos = new Position(pos.line + 1, 0);
-        } else {
-            pos = new Position(pos.line, pos.character + 1);
-        }
-        await wait(ms);
-    }
-
-    await wait(1000);
-
-    return pos;
-}
-
-function hoverAt(editor: TextEditor, position: Position) {
-    editor.selection = new Selection(position, position);
-    vscode.commands.executeCommand('editor.action.showHover');
+async function info(message: string) {
+    output.appendLine(message);
+    await wait(500);
 }
 
 suite('Extension Test Suite', () => {
     after(() => {
-        vscode.window.showInformationMessage('All tests done!');
+        window.showInformationMessage('All tests done!');
+    });
+
+    beforeEach(() => {
+        commands.executeCommand('workbench.action.closeAllEditors');
+    });
+
+    test('Ethers Mode commands should be available only in Ether Mode lang', async () => {
+        await info('`Ethers Mode` commands should not be available when no editor is open');
+        await commands.executeCommand('workbench.action.quickOpen', '>Ethers Mode: ');
+        await wait(2000);
+        await commands.executeCommand('workbench.action.closeQuickOpen');
+
+        await info('`Ethers Mode` commands should not be available when editor is not `ethers` language');
+        await showDocument('plaintext');
+        await commands.executeCommand('workbench.action.quickOpen', '>Ethers Mode: ');
+        await wait(2000);
+        await commands.executeCommand('workbench.action.closeQuickOpen');
+
+        await info('Instead, `Ethers Mode` commands should be available when editor is `ethers` language');
+        await showDocument();
+        await commands.executeCommand('workbench.action.quickOpen', '>Ethers Mode: ');
+        await wait(2000);
+        await commands.executeCommand('workbench.action.closeQuickOpen');
     });
 
     test('Sample test', async () => {
-        const document = await vscode.workspace.openTextDocument({ language: 'ethers' });
-        const editor = await vscode.window.showTextDocument(document);
+        const editor = await showDocument();
 
         let pos = new Position(0, 0);
         pos = await typewrite(editor, pos, `\n`);
@@ -60,3 +61,39 @@ suite('Extension Test Suite', () => {
         assert.strictEqual(-1, [1, 2, 3].indexOf(0));
     });
 });
+
+function wait(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function typewrite(editor: TextEditor, start: Position, text: string, ms = 80) {
+    let pos = start;
+    for (const c of text) {
+        await editor.edit(
+            function (b: TextEditorEdit) {
+                b.insert(pos, c);
+            }
+        );
+        if (c === '\n') {
+            pos = new Position(pos.line + 1, 0);
+        } else {
+            pos = new Position(pos.line, pos.character + 1);
+        }
+        await wait(ms);
+    }
+
+    await wait(1000);
+
+    return pos;
+}
+
+function hoverAt(editor: TextEditor, position: Position) {
+    editor.selection = new Selection(position, position);
+    commands.executeCommand('editor.action.showHover');
+}
+
+async function showDocument(language = 'ethers') {
+    const document = await workspace.openTextDocument({ language });
+    const editor = await window.showTextDocument(document);
+    return editor;
+}
